c3e8bebb0eb35d54d17e3736ddee51bbf9886fad
seonhyeong <752312@naver.com>
2021-02-19 13:59:05 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+
+const config = require('./config/key')
+const { User } = require("./models/User") //User model 가져오기
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+//application/json
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://seonhyeong:qwertz~556@boilerplate.awzhq.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+
+mongoose.connect(config.mongoURI, {
   useNewUrlParser : true , useUnifiedTopology : true , useCreateIndex : true , useFindAndModify : false
 }).then(() => console.log('MongooDB Connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+  //회원 가입 할때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 비에스에 넣어준다.
+  const user = new User(req,bodyParser)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success : false, err})
+    return res.status(200).json({
+      success : true
+    })
+  })
 })
 
 app.listen(port, () => {
