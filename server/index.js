83f16cf06e58beef017150df308edb8f42f5c2be
seonhyeong <752312@naver.com>
2021-02-24 15:24:02 +0900

    Proxy , Concurrently

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 5000
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 
@@ -23,6 +22,8 @@ mongoose.connect(config.mongoURI, {
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/api/hello', (req, res) => res.send('Hello World!~~ 안녕하세요~~~!'))
 
 app.post('/api/users/register', (req, res) => {
   //회원 가입 할때 필요한 정보들을 client에서 가져오면
@@ -94,6 +95,12 @@ app.get('/api/users/logout', auth, (req,res) => {
     })
   })
 })
+
+
+
+
+
+const port = 5000
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
